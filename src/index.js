a355c046c95350e623df39a1f6339a1773702a34
hassan <hchoudhury20@hotmail.com>
2023-07-07 13:04:35 +0100

    initalise game state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import Ship from "./btls";
 import GameBoard from "./gameboard";
 import player from "./player";
 
-const bot = new player("bot")
+function gameloop(){
+const p1= new player("yee")
+const bot = new player ("alien")
+const board1 = new GameBoard(10,10)    
+const board2 = new GameBoard(10,10) 
 
-const wee = new GameBoard(10, 10);
-wee.createGameBoard()
-bot.robot(wee)
+const carrier1 = new Ship(5);
+const battleship1 = new Ship(4);
+const cruiser1 = new Ship(3);
+const submarine1 = new Ship(3);
+const destroyer1 = new Ship(2);
 
-console.log(wee.board);
+const carrier2 = new Ship(5);
+const battleship2 = new Ship(4);
+const cruiser2 = new Ship(3);
+const submarine2 = new Ship(3);
+const destroyer2 = new Ship(2);
+
+
+
+}
